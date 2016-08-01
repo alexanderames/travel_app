1ee1545452b38f6533a3912a2d1aeba44d9bf595
Alexander Ames <alexanderthomasames@gmail.com>
2016-08-01 19:27:55 +0000

    Add country array. Add images. Shoe size string query.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @homestate = 'Arizona'
+    @countries = ['Spain', 'Italy', 'New_Zealand']
+    @images = ['madrid.jpg', 'newZ.jpg', 'venice.jpg']
   end
 
   def about
+    @color = params[:color]
+    @shoe_size = params[:size].to_i
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
 
-  get 'welcome/about'
+  get 'index' => 'welcome#index'
+
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
